0ecb5f6484ddc4c1a35c75c64708afd73cc6c069
Mohamed Khaled <mohamedkhaled7726@gmail.com>
2026-06-30 21:22:01 +0300

    Apply DDT using json to Login Test

---
+++ src/main/java/utils/JsonReader.java
@@ -0,0 +1,39 @@
+package utils;
+
+import com.jayway.jsonpath.JsonPath;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.FileReader;
+
+public class JsonReader {
+
+    String jsonReader;
+    String jsonFileName;
+    private final String testDataPath="src/test/java/testdata/";
+
+    //constructor
+    public JsonReader(String jsonFileName)
+    {
+        this.jsonFileName = jsonFileName;
+        try {
+            JSONObject data = (JSONObject) new JSONParser().parse(new FileReader(testDataPath+jsonFileName+".json"));
+            jsonReader = data.toJSONString();
+        }catch (Exception e)
+        {
+            System.out.println("Error reading json file");
+        }
+    }
+    public String getJsonData(String jsonPath)
+    {
+        try {
+            return JsonPath.read(jsonReader,jsonPath);
+        }
+        catch (Exception e)
+        {
+            System.out.println("Error reading json file");
+            return "";
+        }
+    }
+
+}
